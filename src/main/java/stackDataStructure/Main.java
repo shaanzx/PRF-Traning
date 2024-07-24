24c781c40e2b55daa1dcf60d21ba8315f75c069f
Shanzx <anuhaslakshan07@gmail.com>
2024-07-24 11:16:59 +0530

    Stack data delete done.

--- src/main/java/stackDataStructure/Main.java
+++ src/main/java/stackDataStructure/Main.java
@@ -2,53 +2,57 @@ package stackDataStructure;
 
 public class Main {
     public static void main(String[] args) {
-        //Create a Stack
+        // Create a Stack
         Main add = new Main(5);
-        add.push(10);
-        add.push(20);
-        add.push(30);
-        add.push(40);
-        add.push(50);
+        add.push("I");
+        add.push("j");
+        add.push("s");
+        add.push("e");
         add.printStack();
-        add.pop();
+        //add.pop();
         add.printStack();
     }
+
     int capacity;
     int top;
-    int [] arr;
-    int start;
+    String[] arr;
 
-    Main(int size){
+    Main(int size) {
         capacity = size;
         top = -1;
-        arr = new int[capacity];
-        start = 0;
+        arr = new String[capacity];
     }
-    boolean isFull(){
-        return top == capacity-1;
+
+    boolean isFull() {
+        return top == capacity - 1;
     }
-    void push(int data){
-        if(isFull()){
+
+    void push(String data) {
+        if (isFull()) {
             System.out.println("Stack is full");
-        }else {
+        } else {
             arr[++top] = data;
-            System.out.println(arr[top]+" added successfully.");
+            System.out.println(arr[top] + " added successfully.");
         }
     }
-    boolean isEmpty(){
+
+    boolean isEmpty() {
         return top == -1;
     }
-    void pop(){
-        if(isEmpty()){
+
+    void pop() {
+        if (isEmpty()) {
             System.out.println("Stack is empty");
-        }else {
-            System.out.println(arr[top]+" remove successfully.");
-            arr[top--] = 0;
+        } else {
+            System.out.println(arr[top] + " removed successfully.");
+            arr[top--] = null;
         }
     }
-    void printStack(){
-        for(int i = 0;i<top;i++){
-            System.out.print(arr[i]+" ");
+
+    void printStack() {
+        for (int i = 0; i <= top; i++) {
+            System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 }
