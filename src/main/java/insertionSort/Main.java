a8a77a810aeeda5fa306c7cf0c45b627277356c4
Shanzx <anuhaslakshan07@gmail.com>
2024-07-30 09:25:24 +0530

    Reverse Array Done.

--- src/main/java/insertionSort/Main.java
+++ src/main/java/insertionSort/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("Input Number: ");
         int number = input.nextInt();
 
-        int[] arr = { 5,9,10,7,1,6,4,3,2,8};
+        int[] arr = {5,9,10,7,1,6,4,3,2,8};
         for (int i = 0; i < arr.length; i++) {
             if (number == arr[i]) {
                 int temp = arr[i];
---
+++ src/main/java/reverseArray/Main.java
@@ -0,0 +1,19 @@
+package reverseArray;
+
+import java.util.Arrays;
+/*reverse Array
+ */
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {3,5,7,9,2,13};
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
